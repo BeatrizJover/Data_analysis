bd23182d5981d77554840c6a5a2c01923e3a6195
MiroFronhoffs <miro.fronhoffs@icloud.com>
2024-11-22 09:42:20 +0100

    improved price per m2

--- main.py
+++ main.py
@@ -1,5 +1,3 @@
-import copy
-
 import pandas as pd
 
 belgium_df = pd.read_csv('csv-data/cleaned_dataset_analysis.csv')
@@ -14,15 +12,15 @@ flanders_i = []
 brussels_i = []
 walloon_i = []
 
-for i, row in belgium_df.iterrows():
-    if row["Province"] in flanders:
+for i, rows in belgium_df.iterrows():
+    if rows["Province"] in flanders:
         flanders_i.append(i)
-    elif row["Province"] in brussels:
+    elif rows["Province"] in brussels:
         brussels_i.append(i)
-    elif row["Province"] in walloon:
+    elif rows["Province"] in walloon:
         walloon_i.append(i)
     else:
-        print(F"ERROR {row}")
+        print(F"ERROR {rows}")
 
 flanders_df = belgium_df.iloc[flanders_i,:]
 brussels_df = belgium_df.iloc[brussels_i,:]
@@ -50,9 +48,11 @@ def average_price_municipality(data, most=False, least=False):
         average_price = total / count
         municipality_average_price[locality] = average_price
     if most:
-        return max(municipality_average_price, key=municipality_average_price.get)
+        most_expensive = max(municipality_average_price, key=municipality_average_price.get)
+        return most_expensive, municipality_average_price[most_expensive]
     if least:
-        return min(municipality_average_price, key=municipality_average_price.get)
+        least_expensive = min(municipality_average_price, key=municipality_average_price.get)
+        return least_expensive, municipality_average_price[least_expensive]
     else:
         return municipality_average_price
 
@@ -61,22 +61,23 @@ def median_price_municipality(data, most=False, least=False):
     for group in data:
         locality = group['Locality'].iloc[0]
         temp_df = group[group['Locality'] == locality]
-        municipality_median_price[locality] = temp_df['Price'].median()
+        municipality_median_price[locality] = int(temp_df['Price'].median())
     if most:
-        return max(municipality_median_price, key=municipality_median_price.get)
+        most_expensive = max(municipality_median_price, key=municipality_median_price.get)
+        return most_expensive, municipality_median_price[most_expensive]
     if least:
-        return min(municipality_median_price, key=municipality_median_price.get)
+        least_expensive = min(municipality_median_price, key=municipality_median_price.get)
+        return least_expensive, municipality_median_price[least_expensive]
     else:
         return municipality_median_price
 
 
 
-def price_m2_municipality(data, terrace:bool=False,garden:bool= False, most=False, least=False):
+def price_m2_municipality(data, terrace:bool=True,garden:bool= True,land_surface:bool=True, most=False, least=False):
     municipality_price_m2 = {}
     for group in data:
         meter_squared = 0
         price = 0
-        count = 0
         locality = group['Locality'].iloc[0]
         for _, row in group.iterrows():
             meter_squared += row['Livable Space (m2)']
@@ -84,30 +85,34 @@ def price_m2_municipality(data, terrace:bool=False,garden:bool= False, most=Fals
                 meter_squared += row["Terrace Area (m2)"]
             if garden:
                 meter_squared += row["Garden Area (m2)"]
+            if land_surface:
+                meter_squared += row["Surface of the Land (m2)"]
             price += row['Price']
-            count += 1
-        price_m2 = (meter_squared / price) * count
+        price_m2 = (price/meter_squared)
+        price_m2 = round(price_m2,2)
         municipality_price_m2[locality] = price_m2
     if most:
-        return max(municipality_price_m2, key=municipality_price_m2.get)
+        most_expensive = max(municipality_price_m2, key=municipality_price_m2.get)
+        return most_expensive, municipality_price_m2[most_expensive]
     if least:
-        return min(municipality_price_m2, key=municipality_price_m2.get)
+        least_expensive = min(municipality_price_m2, key=municipality_price_m2.get)
+        return least_expensive, municipality_price_m2[least_expensive]
     else:
         return municipality_price_m2
 
 
 
 
-voi_dict = {"average_price": None, "median_price": None, "price_m2": None}
 
-municipality_voi = {"most_expensive_belgium": copy.deepcopy(voi_dict),
-                    "least_expensive_belgium": copy.deepcopy(voi_dict),
-                    "most_expensive_flanders": copy.deepcopy(voi_dict),
-                    "least_expensive_flanders": copy.deepcopy(voi_dict),
-                    "most_expensive_walloons": copy.deepcopy(voi_dict),
-                    "least_expensive_walloons": copy.deepcopy(voi_dict),
-                    "most_expensive_brussels": copy.deepcopy(voi_dict),
-                    "least_expensive_brussels": copy.deepcopy(voi_dict),}
+
+municipality_voi = {"most_expensive_belgium": {},
+                    "least_expensive_belgium": {},
+                    "most_expensive_flanders": {},
+                    "least_expensive_flanders": {},
+                    "most_expensive_walloons": {},
+                    "least_expensive_walloons": {},
+                    "most_expensive_brussels": {},
+                    "least_expensive_brussels": {}}
 
 municipality_voi["most_expensive_belgium"]["average_price"] = average_price_municipality(belgium_df_list, most=True)
 municipality_voi["most_expensive_belgium"]["median_price"] = median_price_municipality(belgium_df_list, most=True)
@@ -147,3 +152,4 @@ municipality_voi_df = pd.DataFrame(municipality_voi)
 print(municipality_voi_df)
 
 municipality_voi_df.to_csv('csv-data/result.csv')
+
